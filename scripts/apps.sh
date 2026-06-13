3f4f709a8f63c1dadefe6224a833753d850ee44a
Aaron Lai <aaronlai@Aarons-Laptop.local>
2026-06-13 14:40:11 +1000

    feat: brew trust 3rd party formulas

--- scripts/apps.sh
+++ scripts/apps.sh
@@ -4,6 +4,13 @@ set -e
 echo "📦 Installing Brew packages..."
 
 brew update
+
+echo "🤝 Trusting third-party taps..."
+brew trust FelixKratz/formulae
+brew trust koekeishiya/formulae
+brew trust BarutSRB/tap
+brew trust webstonehq/tap
+
 brew bundle --file Brewfile
 
 echo "✅ Apps installed"
